bba2a362db565475b85aecd3aad62e7215b595a7
anasahammad <anasahammadsarker@gmail.com>
2024-03-16 01:19:46 +0600

    basic design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
 
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <Main></Main>
     
     </div>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,16 +6,16 @@ import people from '/images/People.svg'
 
 const Header = () => {
     return (
-        <header>
+        <header className='lexend'>
 
           {/* Navbar Start */}
             <div className="navbar bg-base-100 font-lexend">
   <div className="navbar-start">
     <div className="dropdown">
-      <div tabindex="0" role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabindex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-[#150b2bb3]">
+      <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-[#150b2bb3]">
         <li><a>Home</a></li>
         <li>
           <a>Recipes</a>
@@ -52,14 +52,14 @@ const Header = () => {
         className="py-2 px-4  focus:outline-none rounded-[50px]"
       />
       <button type="submit" className=" text-left px-4">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
          
         </svg>
       </button>
     </div>
 </div>
 
-  <div tabindex="0" role="button" className="btn btn-ghost btn-circle avatar">
+  <div tabIndex="0" role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img alt="Tailwind CSS Navbar component" src={people} />
         </div>
